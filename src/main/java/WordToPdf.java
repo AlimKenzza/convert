0137edbc262e5105bd881adf95d0fef1f29dcb55
aimangazin <alimkenz02@gmail.com>
2022-05-20 11:07:16 +0600

    changes

--- src/main/java/WordToPdf.java
+++ src/main/java/WordToPdf.java
@@ -5,7 +5,7 @@ public class WordToPdf {
     public static void main(String[] args) {
         //Загрузить образец документа Word
         Document document = new Document();
-        document.loadFromFile("C://Users/Alimzhan/IdeaProjects/untitled2/src/main/resources/files/edocument.docx");
+        document.loadFromFile("C://Users/Alimzhan/IdeaProjects/untitled2/src/main/resources/files/convert.docx");
 
 
         //Сохранить как PDF
