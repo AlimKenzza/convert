960c4ca26a2879469362b37cdb48686fc6098a40
aimangazin <alimkenz02@gmail.com>
2022-05-20 15:18:53 +0600

    commit

--- src/main/java/kz/aitu/WordToPdf.java
+++ src/main/java/kz/aitu/WordToPdf.java
@@ -1,11 +1,10 @@
+package kz.aitu;
+
 import com.spire.doc.Document;
 import com.spire.doc.FileFormat;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class WordToPdf {
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) {
         //Загрузить образец документа Word
         Document document = new Document();
         document.loadFromFile("src/main/resources/files/convert.docx");
